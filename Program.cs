b2c7c9315970a72e7202fe04f763a1075b500dc0
EvgeniySh77 <evgen.sh77@mail.ru>
2021-12-31 18:17:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task_4._1._17
+{
+    class MainClass
+    {
+        static void Main()
+        {
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+			var color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			}
+
+			else if (color == "green")
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+			}
+
+			Console.ReadKey();
+		}
+    }
+}
